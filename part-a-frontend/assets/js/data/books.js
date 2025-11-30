1b44293620cd7d6b1024f96060515fa7533ac0f8
harabalos <babismetaxas000@gmail.com>
2025-11-30 09:26:35 +0200

    made some books

---
+++ part-a-frontend/assets/js/data/books.js
@@ -0,0 +1,27 @@
+// AICANARY: CSD-ELearn-2025
+const booksData = [
+    {
+        id: 101,
+        title: "Clean Code",
+        category: "Programming",
+        price: "35€",
+        image: "assets/img/thumbnails/book1.jpg",
+        desc: "Ο απόλυτος οδηγός για καθαρό και συντηρήσιμο κώδικα."
+    },
+    {
+        id: 102,
+        title: "Learning React",
+        category: "Web",
+        price: "40€",
+        image: "assets/img/thumbnails/book2.jpg",
+        desc: "Μάθετε τη δημοφιλή βιβλιοθήκη της Facebook από το μηδέν."
+    },
+    {
+        id: 103,
+        title: "The DevOps Handbook",
+        category: "DevOps",
+        price: "45€",
+        image: "assets/img/thumbnails/book3.jpg",
+        desc: "Πώς να ενώσετε το Development με το Operations."
+    }
+];
